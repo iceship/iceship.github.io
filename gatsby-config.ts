2dda78af0ae8553d59705ac21c0508e8dfa103b6
iceship <iceship@gmail.com>
2024-08-05 21:26:32 +0900

    g-tag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,25 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-postcss", {
+    resolve: "gatsby-plugin-google-gtag",
+    options: {
+      trackingIds: ['G-87X1YHJGRK'],
+      gtagConfig: {
+        anonymize_ip: true,
+        cookie_expires: 0,
+      },
+      pluginConfig: {
+        head: false,
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays processing pageview events on route update (in milliseconds)
+        delayOnRouteUpdate: 0,                
+      },
+    },
+  },     
+    "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
